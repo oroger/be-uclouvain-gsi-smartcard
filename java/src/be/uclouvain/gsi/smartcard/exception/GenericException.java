e7aa7539e9ebc7869fed1cb9a8bffc8fc4691d34
pom2pin <pom2pin@e82258de-cefa-11de-9e88-ff79fe1e0c1e>
2010-06-09 18:05:53 +0000

    first branch [Test]

--- java/src/be/uclouvain/gsi/smartcard/exception/GenericException.java
+++
@@ -1,8 +0,0 @@
-package be.uclouvain.gsi.smartcard.exception;
-
-@SuppressWarnings("serial")
-public class GenericException extends Exception{
-	public GenericException(String msg) {
-		super(msg);
-	}
-}
--- java/src/be/uclouvain/gsi/smartcard/exception/ReaderException.java
+++
@@ -1,8 +0,0 @@
-package be.uclouvain.gsi.smartcard.exception;
-
-@SuppressWarnings("serial")
-public class ReaderException extends GenericException{
-	public ReaderException(String msg) {
-		super(msg);
-	}
-}
